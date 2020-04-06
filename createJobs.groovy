e28d16377698dc84f52f2369a68b58bca89bfde1
jordanAclarke <jordanclarkemba@gmail.com>
2020-04-06 13:32:23 -0400

    Theme-park-project added

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/JordanAclarke/Theme-parkrides-jenkinsintegration.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
